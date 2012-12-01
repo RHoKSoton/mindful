978606f940ed392797f418c11bff66f1baa071bf
Oliver Wilkie <osw09@ic.ac.uk>
2012-12-01 14:26:39 +0000

    Added mindfulapp to django admin. Small model changes were needed

---
+++ mindfulapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from models import *
+
+admin.site.register(Patient)
+admin.site.register(Genre)
+admin.site.register(Song)
+admin.site.register(Listen)
+admin.site.register(Carer)
+admin.site.register(Observation)
+
