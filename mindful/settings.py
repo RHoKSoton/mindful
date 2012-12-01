8d0a3d521de5423325b29d1d525f3f9edf03cfe4
David <david@ubuntu.(none)>
2012-12-01 15:27:02 +0000

    Added first template

--- mindful/settings.py
+++ mindful/settings.py
@@ -1,4 +1,7 @@
+import os
 # Django settings for mindful project.
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -106,6 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	 PROJECT_PATH + '/mindfulapp/templates'
 )
 
 INSTALLED_APPS = (
--- mindfulapp/views.py
+++ mindfulapp/views.py
@@ -1,4 +1,5 @@
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello World")
+	return render_to_response('index.html')
