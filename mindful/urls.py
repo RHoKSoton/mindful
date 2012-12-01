6b93f208795b3784b9b2adb49decc222c1bfee88
David <david@ubuntu.(none)>
2012-12-01 14:38:56 +0000

    Created first view and added url mapping for specific application

--- mindful/urls.py
+++ mindful/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mindful.views.home', name='home'),
-    # url(r'^mindful/', include('mindful.foo.urls')),
+    url(r'^$', include('mindfulapp.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
